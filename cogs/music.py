31d85d29b9c5fe788da3a26bc586e663846e1861
nath1100 <Nath.Luu1@gmail.com>
2017-04-24 17:18:19 +1000

    Tweaked the !k.play command
    Fixed stream skipping the first second of the song.
    Fixed consecutive calls of !k.play causing the player to break.

--- cogs/music.py
+++ cogs/music.py
@@ -1,4 +1,4 @@
-import discord
+import discord, asyncio
 from discord.ext import commands
 
 class Music:
@@ -17,16 +17,22 @@ class Music:
             else:
                 # if user is connected to a voice channel and bot is not in a voice channel already:
                 voice_client = await self.bot.join_voice_channel(ctx.message.author.voice.voice_channel)
+                # Note: no player is created upon join. A song must be played for a player to be registered to self.player
         else:
             await self.bot.say("I am already connected to a voice channel...")
 
     @commands.command(pass_context=True)
     async def leave(self, ctx):
         """Get Kamikaze to leave the voice channel she is connected in"""
-        if self.bot.is_voice_connected(ctx.message.server):
-            voice_client = self.bot.voice_client_in(ctx.message.server)
+        server = ctx.message.server
+        if self.bot.is_voice_connected(server):
+            voice_client = self.bot.voice_client_in(server)
             await self.bot.say("Disconnecting from {}".format(voice_client.channel.name))
             await voice_client.disconnect()
+            try:
+                self.players.pop(server.id)
+            except KeyError:
+                pass
         else:
             await self.bot.say("Not connected to any voice channel...")
 
@@ -36,7 +42,12 @@ class Music:
         server = ctx.message.server
         if self.bot.is_voice_connected(server):
             voice_client = self.bot.voice_client_in(server)
+            try:
+                self.players[server.id].stop()
+            except KeyError:
+                pass
             self.players[server.id] = await voice_client.create_ytdl_player(url)
+            await asyncio.sleep(1)
             self.players[server.id].start()
             await self.bot.delete_message(ctx.message)
             await self.bot.say("Playing **{}**".format(self.players[server.id].title))
@@ -76,10 +87,23 @@ class Music:
         try:
             if self.players[server.id].is_playing():
                 self.players[server.id].stop()
+                await self.bot.say("Stopped playing **{}**".format(self.players[server.id].title))
             else:
                 await self.bot.say("Currently not playing anything...")
         except KeyError:
             await self.bot.say("Currently not connected to any voice channel.")
+
+    @commands.command(pass_context=True)
+    async def np(self, ctx):
+        """Get the currently playing song's title"""
+        server = ctx.message.server
+        try:
+            if not self.players[server.id].is_done():
+                await self.bot.say("Currently playing **{}**".format(self.players[server.id].title))
+            else:
+                await self.bot.say("Currently not playing anything...")
+        except KeyError:
+            await self.bot.say("Currently not conencted to any voice channel.")
 
 def setup(bot):
     bot.add_cog(Music(bot))
